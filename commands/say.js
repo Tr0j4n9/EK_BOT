5e292f07ad4309ed6793c2458faa698845eb6076
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-20 19:43:49 +1300

    Add files via upload

---
+++ commands/say.js
@@ -0,0 +1,17 @@
+
+module.exports = {
+  main: (bot, msg, settings) => {
+
+  	// makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+message.channel.send(sayMessage);
+
+  },
+  args: '<string>',
+  help: 'Boop another person in your server',
+  hide: false
+}
