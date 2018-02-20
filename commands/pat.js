20893be62ee13058448cbcf92d0d29d6bb0f07cc
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-21 06:53:54 +1300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -6,6 +6,9 @@ module.exports = {
     var Discord = require("discord.js");
     
     
+    var obj = JSON.parse(text); 
+    obj.employees[links].link;
+    
     
     var patGifs = ['https://cdn.discordapp.com/attachments/287305378314846208/415429776069623809/pat-SyFmqkFwW.gif', 'https://cdn.discordapp.com/attachments/287305378314846208/415430123454464010/pat-BkJBQlckz.gif',
                   'https://cdn.discordapp.com/attachments/287305378314846208/415430143411224577/pat-SJva1kFv-.gif', 'https://cdn.discordapp.com/attachments/287305378314846208/415430166735618058/pat-HJGQlJYwb.gif',
@@ -18,7 +21,7 @@ module.exports = {
 
         
   	msg.channel.send("Hey " + (msg) + ", you got patted!");
-    msg.channel.send(patGifs);
+    msg.channel.send(links);
     console.log((msg) + " got patted.")
   },
   args: '<@mention>',
