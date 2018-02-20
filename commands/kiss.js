36424ab62b55d6f4237633d2e3993edb22ab18a6
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-21 09:41:33 +1300

    Update kiss.js

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,1 +1,11 @@
-
+module.exports = {
+  main: (bot, msg, settings) => {
+    
+    var Discord = require("discord.js");
+  	msg.channel.send("Hey " + (msg) + ", you got nomed! :patato:");
+    console.log((msg) + " got cookied.")
+  },
+  args: '<@mention>',
+  help: 'nom another person in the server',
+  hide: false
+}
