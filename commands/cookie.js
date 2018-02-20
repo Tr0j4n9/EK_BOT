8d0d88232f2a18dfe7756f6a102ae3170d14b898
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-21 08:46:15 +1300

    Create cookie.js

---
+++ commands/cookie.js
@@ -0,0 +1,11 @@
+module.exports = {
+  main: (bot, msg, settings) => {
+    
+    var Discord = require("discord.js");
+  	msg.channel.send("Hey " + (msg) + ", you got cookied! :cookie:");
+    console.log((msg) + " got cookied.")
+  },
+  args: '<@mention>',
+  help: 'Cookie another person in your server',
+  hide: false
+}
