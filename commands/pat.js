42b708a0065beb2058374de151bbb9d75732758b
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-20 22:06:39 +1300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -4,15 +4,17 @@ module.exports = {
     var Discord = require("discord.js");
     
     
-    var randomValue = patGifs[Math.floor(Math.random() * patGifs.length)];
     
     var patGifs = ['https://cdn.discordapp.com/attachments/287305378314846208/415429776069623809/pat-SyFmqkFwW.gif', 'https://cdn.discordapp.com/attachments/287305378314846208/415430123454464010/pat-BkJBQlckz.gif',
                   'https://cdn.discordapp.com/attachments/287305378314846208/415430143411224577/pat-SJva1kFv-.gif', 'https://cdn.discordapp.com/attachments/287305378314846208/415430166735618058/pat-HJGQlJYwb.gif',
                   'https://cdn.discordapp.com/attachments/287305378314846208/415430618940178454/pat-Sk2FyQHpZ.gif'];
     
-    var show = patGifs[Math.floor(Math.random() * patGifs.length)];
+
+    Math.floor(Math.random(mathran1) * 6);
     
     
+
+        
   	msg.channel.send("Hey " + (msg) + ", you got patted!");
     msg.channel.send(patGifs);
     console.log((msg) + " got patted.")
