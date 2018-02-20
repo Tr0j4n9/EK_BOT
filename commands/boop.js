a978eb8f8aa11edb4af99cd2b73893598a3ddb08
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-20 20:46:29 +1300

    Update boop.js

--- commands/boop.js
+++ commands/boop.js
@@ -2,10 +2,10 @@ module.exports = {
   main: (bot, msg, settings) => {
     
     var Discord = require("discord.js");
-  	msg.channel.sendMessage("Hey" + (msg) + "," + (sender) + "Booped you." , msg);
+  	msg.channel.sendMessage("Hey" + (msg) + "," + (username) + "Booped you." , msg);
 
   },
-  args: '<string>',
+  args: '<@mention>',
   help: 'Boop another person in your server',
   hide: false
 }
