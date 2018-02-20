d978834cad131e52b505769c99211ec89b63149a
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-21 07:28:38 +1300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -22,9 +22,11 @@ module.exports = {
                 total++;
                 var obj_keys = Object.keys(pats);
                 var rnd_key = obj_keys[Math.floor(Math.random() *obj_keys.length)];
-                msg.reply(pats[rnd_key].spanreply);
-        msg.channel.send("Hey " + (msg) + ", you got pat!");
-            console.log((msg) + " got pat!")
+
+                msg.channel.send("Hey " + (msg) + ", you got pat!");
+                msg.chanel.send(pats[rnd_key].spanreply);
+        
+                console.log((msg) + " got pat!")
     }
 
     console.log((msg) + " got patted.")
