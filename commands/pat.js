9954625ba42f5cbe8037a628659ca66dd98f513a
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-20 21:57:58 +1300

    Update pat.js

--- commands/pat.js
+++ commands/pat.js
@@ -2,10 +2,22 @@ module.exports = {
   main: (bot, msg, settings) => {
     
     var Discord = require("discord.js");
-  	msg.channel.send("Hey " + (msg) + ", you got booped!");
-    console.log((msg) + " got booped.")
+    
+    
+    var randomValue = patGifs[Math.floor(Math.random() * patGifs.length)];
+    
+    var patGifs = ['https://cdn.discordapp.com/attachments/287305378314846208/415429776069623809/pat-SyFmqkFwW.gif', 'https://cdn.discordapp.com/attachments/287305378314846208/415430123454464010/pat-BkJBQlckz.gif',
+                  'https://cdn.discordapp.com/attachments/287305378314846208/415430143411224577/pat-SJva1kFv-.gif', 'https://cdn.discordapp.com/attachments/287305378314846208/415430166735618058/pat-HJGQlJYwb.gif',
+                  'https://cdn.discordapp.com/attachments/287305378314846208/415430618940178454/pat-Sk2FyQHpZ.gif'];
+    
+    var show = patGifs[Math.floor(Math.random() * patGifs.length)];
+    
+    
+  	msg.channel.send("Hey " + (msg) + ", you got patted!");
+    msg.channel.send(patGifs);
+    console.log((msg) + " got patted.")
   },
   args: '<@mention>',
-  help: 'Boop another person in your server',
+  help: 'Pat another person in your server',
   hide: false
 }
