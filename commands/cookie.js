3b60a2ab1aa5ce978157e13a6a39da18d946b7a3
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-21 21:11:18 +1300

    Update cookie.js

--- commands/cookie.js
+++ commands/cookie.js
@@ -2,7 +2,7 @@ module.exports = {
   main: (bot, msg, settings) => {
     
     var Discord = require("discord.js");
-  	msg.channel.send("Hey " + (msg) + ", " + (msg.author) + ", you got cookied! :cookie:");
+  	msg.channel.send("Hey " + (msg) + ", " + (msg.author) + ", cookied you! :cookie:");
     console.log((msg) + " got cookied.")
   },
   args: '<@mention>',
