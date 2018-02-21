5cb5260802ee56f028f49478509f9dad78abad3f
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-21 21:45:31 +1300

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+  main: (bot, msg, settings) => {
+	msg.reply("Just kidding, this command does not work yet... PM @nullariel#6948 to give some feedback. :hearts:");
+  },
+  args: '<string>',
+  help: '**Feedback command**',
+  hide: false
+}
