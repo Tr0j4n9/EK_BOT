992fa6cd1b15abedb827aca84e9fcaf9ff461180
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-22 16:37:18 +1300

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -2,7 +2,8 @@ module.exports = {
   main: (bot, msg, settings) => {
 	  
 	  var economy = require('discord-eco');
-
+	  
+	  economy.updateBalance(msg.author.id, 500).then((i) => {
 	  
 	  msg.channel.send(`**You got $500!**\n**New Balance:** ${i.money}`);
 	  
