8099a177d336de01d1ba78bdbbd387c1992e9035
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-22 16:07:05 +1300

    Create daily.js

---
+++ commands/daily.js
@@ -0,0 +1,19 @@
+            economy.updateBalance(message.author.id, 500).then((i) => { // economy.updateBalance grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
+                message.channel.send(`**You got $500!**\n**New Balance:** ${i.money}`);
+                
+                
+  
+  
+  
+  module.exports = {
+  main: (bot, msg, settings) => {
+	
+  
+  economy.updateBalance(message.author.id, 100).then((i) => { // economy.updateBalance grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
+  message.channel.send(`**You got $100!**\n**New Balance:** ${i.money}`);
+  
+  },
+  args: 'no args',
+  help: 'Claim daily coins!',
+  hide: false
+}
