69dff634454c852928c5262d85a28ad3c6ddc5c2
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-22 16:01:26 +1300

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -1,7 +1,8 @@
 
   module.exports = {
   main: (bot, msg, settings) => {
-
+    
+    var economy = require('discord-eco');
     economy.fetchBalance('userID').then((i) => {
     	console.log(i) // { userID: '144645791145918464', money: 998, lastDaily: 'Not Collected' }
         console.log(i.money) // 998
