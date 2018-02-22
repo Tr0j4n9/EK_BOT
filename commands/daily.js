c2f1d4c518c3159e39e8a75af778f47f1e088b10
Tr0j4n9 <30397502+Tr0j4n9@users.noreply.github.com>
2018-02-22 18:18:02 +1300

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -2,6 +2,7 @@ module.exports = {
   main: (bot, msg, settings) => {
 	  
 	  var economy = require('discord-eco');
+	  var balance = require('./balance.js');
 	  
             economy.updateBalance(msg.author.id, 100).then((i) => { // economy.updateBalance grabs the (userID, value) value being how much you want to add, and puts it into 'i'.
             msg.channel.send(`**You got $100!**\n**New Balance:** ${i.money}`);
